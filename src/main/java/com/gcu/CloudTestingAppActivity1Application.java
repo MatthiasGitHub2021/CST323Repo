42f4f2aa717469698992d4c0a001fc4df8eb1ad8
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2022-04-26 17:15:15 -0700

    Initial commit.

---
+++ src/main/java/com/gcu/CloudTestingAppActivity1Application.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTestingAppActivity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTestingAppActivity1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcu/CloudTestingAppActivity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTestingAppActivity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
