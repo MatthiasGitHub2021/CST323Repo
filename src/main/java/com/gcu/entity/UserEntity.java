fd51a04f246b381349b357e4f2b69b1d725934e8
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2022-05-13 13:22:24 -0700

    Commented out everything but index.html and homeController.

--- src/main/java/com/gcu/entity/UserEntity.java
+++ src/main/java/com/gcu/entity/UserEntity.java
@@ -1,79 +1,79 @@
-package com.gcu.entity;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
-
-/**
- * UserEntity to match database table attributes named cloud_testing in MAMP
- */
-@Table("users")
-public class UserEntity {
-
-    @Id
-    private int id;
-
-    @Column("FirstName")
-    private String firstName;
-
-    @Column("LastName")
-    private String lastName;
-
-    @Column("UserName")
-    private String userName;
-
-    @Column("Password")
-    private String password;
-
-    public UserEntity(int id, String firstName, String lastName, String userName, String password) {
-       super();
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public UserEntity(){}
-
-    //getters/setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
+//package com.gcu.entity;
+//
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.relational.core.mapping.Column;
+//import org.springframework.data.relational.core.mapping.Table;
+//
+///**
+// * UserEntity to match database table attributes named cloud_testing in MAMP
+// */
+//@Table("users")
+//public class UserEntity {
+//
+//    @Id
+//    private int id;
+//
+//    @Column("FirstName")
+//    private String firstName;
+//
+//    @Column("LastName")
+//    private String lastName;
+//
+//    @Column("UserName")
+//    private String userName;
+//
+//    @Column("Password")
+//    private String password;
+//
+//    public UserEntity(int id, String firstName, String lastName, String userName, String password) {
+//       super();
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.userName = userName;
+//        this.password = password;
+//    }
+//
+//    public UserEntity(){}
+//
+//    //getters/setters
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
--- src/main/java/com/gcu/repository/ProductRepository.java
+++ src/main/java/com/gcu/repository/ProductRepository.java
@@ -1,9 +1,9 @@
-package com.gcu.repository;
-
-import com.gcu.entity.ProductEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
-
-
-}
+//package com.gcu.repository;
+//
+//import com.gcu.entity.ProductEntity;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
+//
+//
+//}
