53f2d14a0c85dea326c853fa061ca93402ff9d75
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2022-05-12 18:16:41 -0700

    Roll back to fix database in Azure

--- src/main/java/com/gcu/controllers/HomeController.java
+++ src/main/java/com/gcu/controllers/HomeController.java
@@ -1,17 +1,17 @@
-//package com.gcu.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-///**
-// * Controller for actions related to home/index page
-// */
-//@Controller
-//public class HomeController {
-//
-//    @GetMapping("/")
-//    public String index(){
-//        return "index";
-//    }
-//
-//}
+package com.gcu.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for actions related to home/index page
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
