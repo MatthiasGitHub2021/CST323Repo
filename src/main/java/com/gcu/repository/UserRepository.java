46080a332caf190eec40fbccd49731297600258a
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2022-05-13 13:22:24 -0700

    Commented out everything but index.html and homeController.

--- src/main/java/com/gcu/repository/UserRepository.java
+++ src/main/java/com/gcu/repository/UserRepository.java
@@ -1,9 +1,9 @@
-package com.gcu.repository;
-
-import com.gcu.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
-    UserEntity findByUserName(String userName);
-}
+//package com.gcu.repository;
+//
+//import com.gcu.entity.UserEntity;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface UserRepository extends CrudRepository<UserEntity, Long> {
+//
+//    UserEntity findByUserName(String userName);
+//}
